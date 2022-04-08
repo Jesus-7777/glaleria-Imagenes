a0d2caeb94f0d9d52c5950cb7a84cd8526004e19
Jesus-7777 <61198234+Jesus-7777@users.noreply.github.com>
2022-04-07 20:20:51 -0500

    subieno vaidaciones

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
+from email.mime import image
 from flask import Flask, render_template,request,redirect, url_for, flash, session
 from werkzeug.security import generate_password_hash, check_password_hash
 from config.database import db
 from models import validacionModels
-from models.modelUser import UsuarioModel
+from models.validacionModels import UsuarioModel, obtenerUser
+
 
 
 app = Flask(__name__)
 app.secret_key='asdjkajsdfjerybbca5445asdfafeyrfa'
+
+
 
 @app.get("/")
 def index():
@@ -14,14 +18,50 @@ def index():
     
 @app.route("/login", methods=['GET','POST'])
 def loginUser():
-    if request.method=='POST':
+    """ usuarioModel = UsuarioModel() """
+    if request.method == "GET":
         return render_template("login.html")
     else:
-        return render_template("login.html")
+        usuario = request.form['email']
+        password = request.form['password']
+        paswordCheck=generate_password_hash(password)
+        user= obtenerUser(usuario)
+    
+        for datosss in user:
+            print(datosss['password'])
+            break
+        """ user = cursor.execute('select correo, password from usuario where usuario.correo = %s',(usuario,)) """ 
+        """ result = validardateuser(user,paswordCheck) """
+        if user is not None and user==True:
+            print("Welcome")
+            session['username'] = usuario
+            return redirect(url_for("viewuser"))
+        else:
+            flash("Correo / Password incorrect")
+            return redirect(url_for("loginUser"))
+            
+def validardateuser(users,password):
+    for user in users:
+        return check_password_hash(user[3],password)
+
+@app.get('/')
+def viewUser():
+    return "esto es una pagina"
 
 @app.get("/crear")
 def creaUsuario():
     return render_template("crearUser.html")
+
+@app.get("/imagn")
+def crearImagen():
+    return render_template("/templates/imageloader/fromimage.html")
+
+@app.post("/imagen")
+def crearImagenPost():
+    imagen= request.files['imagen']
+    """ print(imagen.filename)
+    imagen.save('./static/image/'+imagen.filename) """
+    return render_template("/templates/imageloader/fromimage.html")
 
 @app.post("/crear")
 def creaUsuarioPost():
@@ -45,8 +85,9 @@ def creaUsuarioPost():
         flash("El correo es obligatorio")
         """ print("El correo es obligatorio") """
     
-    if password =="":
+    if password =="" or password != None:
         isValid=False
+        validacionModels.validate_password(password)
         flash("La contraseña es obligatorio")
     
     if 8 > len(password) :                       
@@ -60,5 +101,6 @@ def creaUsuarioPost():
     
     validacionModels.crearUser(nombre=nombre,correo=correo,password=passwordEncrypted)
     return redirect(url_for('loginUser'))
+
 
 app.run(debug=True)
--- models/validacionModels.py
+++ models/validacionModels.py
@@ -1,13 +1,24 @@
+from dataclasses import dataclass
 from fileinput import close
+import json
+from tkinter.messagebox import NO
+from werkzeug.security import check_password_hash,generate_password_hash
 import re
 from flask import flash
 from config.database import db
 
+cursor=db.cursor(dictionary=True)
+class UsuarioModel():
+    def validarUser(self,usuario):
+        cursor =db.cursor()
+        cursor.execute('select correo, password from usuario where usuario.correo = %s',(usuario,))      
+        userdata= cursor.fetchall()
+        cursor.close()
+        return userdata
 
-
-def obtenerUser():
+def obtenerUser(usuario):
     cursor=db.cursor(dictionary=True)
-    cursor.execute('select * from usua')
+    cursor.execute('select correo, password from usuario where usuario.correo = %s',(usuario,)) 
     productos= cursor.fetchall()
     cursor= close()
     return productos
@@ -17,6 +28,22 @@ def crearUser(nombre,correo,password):
     cursor.execute("insert into usuario(nombre, correo, password) values(%s,%s,%s)",(nombre,correo,password))
     cursor.close()
     
+
+def usuarioExit(correo,password):
+        print(password)
+        passwordChek=generate_password_hash(password)
+        cursor.execute('SELECT * FROM usuarios WHERE usuario = ? AND clave = ?', (correo, passwordChek))
+        dataBases = cursor.fetchone()
+        
+        if dataBases!=None:
+            for data in dataBases:
+                """ check_password_hash(data[3],passwordChek) """
+            print(data['correo'])
+        else:
+            print("Usuario incorrecto")
+            
+        print("Query Excecuted successfully")
+            
 
 def validate_password(password):
     """J3$u$diaz"""
